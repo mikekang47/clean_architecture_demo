e93cf00e3631baa454491c21cbf0521776bb0790
mikekang47 <gyeong0308@gmail.com>
2022-07-21 14:52:36 +0900

    Set environment

---
+++ src/main/java/com/example/clean_architecture_demo/CleanArchitectureDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.clean_architecture_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchitectureDemoApplication.class, args);
+	}
+
+}
